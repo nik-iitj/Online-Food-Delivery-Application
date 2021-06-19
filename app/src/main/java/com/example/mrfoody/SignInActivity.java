8a961c1fc3ba6312f2a8da48468eb44e77d01128
nik-iitj <raj.14@iitj.ac.in>
2021-06-19 08:57:58 +0530

    Authorization done

---
+++ app/src/main/java/com/example/mrfoody/SignInActivity.java
@@ -0,0 +1,131 @@
+package com.example.mrfoody;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SignInActivity extends AppCompatActivity {
+
+    SignInButton btn;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        btn = findViewById(R.id.signInBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void signIn(){
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task){
+
+        try{
+            GoogleSignInAccount acc = task.getResult(ApiException.class);
+            Toast.makeText(this, "SignIn successful :)", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc);
+
+
+        }
+
+         catch (ApiException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acc){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acc.getIdToken(),null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete( Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else{
+
+                    updateUI(null);
+                    Toast.makeText(SignInActivity.this, "Failed :(", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    private void updateUI(FirebaseUser user){
+
+        if(user!=null){
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+
+            if(account!=null){
+                Intent intent = new Intent(SignInActivity.this,userSelectionActivity.class);
+                startActivity(intent);
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/mrfoody/userSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.mrfoody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_selection);
+    }
+}
